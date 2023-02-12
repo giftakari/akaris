8ec6b54da0fbf308bb8ccdd1c01f74cdc30fc756
giftakari <giftakari@yahoo.com>
2023-02-12 17:40:43 +1100

    configure routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Flight', url: '/folder/Flight', icon: 'airplane' },
+    { title: 'Hotel', url: '/folder/Hotel', icon: 'paper-plane' },
+    { title: 'Cars', url: '/folder/Cars', icon: 'car-sport' },
+    { title: 'Bookings', url: '/folder/Manage-Booking', icon: 'briefcase' },
+    { title: 'Offers', url: '/folder/Offers', icon: 'wallet' },
+    { title: 'Group', url: '/folder/Group-Booking', icon: 'people' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = ['Check-In', 'Subscribe', 'Visa', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
