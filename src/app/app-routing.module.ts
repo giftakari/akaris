fd41cb5f0fb070189eec7ac903996f3d85a648b6
giftakari <giftakari@yahoo.com>
2023-02-13 12:13:39 +1100

    flight routes add more routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,38 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'flights',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+    path: 'flights',
+    loadChildren: () => import('./pages/flights/flights.module').then( m => m.FlightsPageModule)
+  },
+  {
+    path: 'hotels',
+    loadChildren: () => import('./pages/hotels/hotels.module').then( m => m.HotelsPageModule)
+  },
+  {
+    path: 'cars',
+    loadChildren: () => import('./pages/cars/cars.module').then( m => m.CarsPageModule)
+  },
+  {
+    path: 'manage-booking',
+    loadChildren: () => import('./pages/manage-booking/manage-booking.module').then( m => m.ManageBookingPageModule)
+  },
+  {
+    path: 'offers',
+    loadChildren: () => import('./pages/offers/offers.module').then( m => m.OffersPageModule)
+  },
+  {
+    path: 'group-booking',
+    loadChildren: () => import('./pages/group-booking/group-booking.module').then( m => m.GroupBookingPageModule)
+  },
+  {
+    path: 'page-not-found',
+    loadChildren: () => import('./pages/page-not-found/page-not-found.module').then( m => m.PageNotFoundPageModule)
+  },
+
 ];
 
 @NgModule({
